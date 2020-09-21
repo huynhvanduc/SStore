4a788d0b514cf13d2a11340026e8f8d81cdd34d8
nguyenhoangnhon <nguyenhoangnhon@gmail.com>
2020-09-21 15:24:24 +0700

    login 241 done

---
+++ SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Password { set; get; }
+        public string ReturnUrl { set; get; } = "/";
+    }
+}
